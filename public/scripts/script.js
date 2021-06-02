e7cae5df3d6a80fb0f30dc429df5d4dd5c685739
vecume <shahzod.bek200109@gmail.com>
2021-06-02 16:45:48 +0500

    Admin home page completed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,34 +1,14 @@
 try {
-  const header = document.querySelector(".tayoq");
-  const nav = document.querySelector("#main-nav");
+  const nav = $("#main-nav");
 
-  const navHeight = nav.getBoundingClientRect().height;
+  const navHeight = nav.height();
 
   $(".navigation__nav").css(`top`, `${navHeight}px`);
 
   $("body").css(`margin-top`, `${navHeight}px`);
-  // console.log(navHeight)
 
-  // const stickyNav = function (entries) {
-  //   const [entry] = entries;
-  //   // console.log(entry)
-
-  //   if (!entry.isIntersecting) {
-  //     nav.classList.add("sticky");
-  //   } else {
-  //     nav.classList.remove("sticky");
-  //   }
-  // };
-  // const headerObserver = new IntersectionObserver(stickyNav, {
-  //   root: null,
-  //   threshold: 0,
-  //   rootMargin: `-${navHeight}px`,
-  // });
-
-  // headerObserver.observe(header);
-
-  window.addEventListener("scroll", () => {
-    nav.classList.toggle("sticky", window.scrollY > 0);
+  $(window).on("scroll", () => {
+    nav.toggleClass("sticky", window.scrollY > 0);
     if (window.scrollY > 0) {
       $("body").css(`margin-top`, `${0}px`);
     } else {
@@ -60,101 +40,6 @@ try {
       section.classList.remove("section--hidden");
     }
   });
-
-  // Slider
-  // const slider = function () {
-  //   const slides = document.querySelectorAll('.kar');
-  //   const btnLeft = document.querySelector('.slider__btn--left');
-  //   const btnRight = document.querySelector('.slider__btn--right');
-
-  //   const dotContainer = document.querySelector('.dots');
-
-  //   let curSlide = 0;
-  //   const maxSlide = slides.length;
-
-  //   const slider = document.querySelector('.karusel');
-  //   const createDots = function () {
-  //     slides.forEach(function (_, i) {
-  //       dotContainer.insertAdjacentHTML(
-  //         'beforeend',
-  //         `<button class="dots__dot" data-slide="${i}">${i + 1}</button>`
-  //       );
-  //     });
-  //   };
-
-  //   // createDots();
-
-  //   const activateDot = function (slide) {
-  //     document
-  //       .querySelectorAll('.dots__dot')
-  //       .forEach((dot) => dot.classList.remove('dots__dot--active'));
-
-  //     document
-  //       .querySelector(`.dots__dot[data-slide="${slide}"]`)
-  //       .classList.add('dots__dot--active');
-  //   };
-  //   // activateDot(0);
-
-  //   const goToSlide = function (slide) {
-  //     slides.forEach(
-  //       (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-  //     );
-  //   };
-
-  //   // goToSlide(0);
-
-  //   // Next slide
-  //   const nextSlide = function () {
-  //     if (curSlide === maxSlide - 1) {
-  //       curSlide = 0;
-  //     } else {
-  //       curSlide++;
-  //     }
-
-  //     goToSlide(curSlide);
-  //     activateDot(curSlide);
-  //   };
-
-  //   const prevSlide = function () {
-  //     if (curSlide === 0) {
-  //       curSlide = maxSlide - 1;
-  //     } else {
-  //       curSlide--;
-  //     }
-  //     goToSlide(curSlide);
-  //     activateDot(curSlide);
-  //   };
-
-  //   // const init = function () {
-  //   //   goToSlide(0);
-  //   //   createDots();
-  //   //   activateDot(0);
-  //   // };
-
-  //   // init();
-
-  //   // Event handlers
-  //   btnRight.addEventListener('click', nextSlide);
-  //   btnLeft.addEventListener('click', prevSlide);
-
-  //   document.addEventListener('keydown', function (e) {
-  //     // console.log(e);
-
-  //     if (e.key === 'ArrowLeft') prevSlide();
-  //     e.key === 'ArrowRight' && nextSlide();
-  //   });
-
-  //   dotContainer.addEventListener('click', function (e) {
-  //     if (e.target.classList.contains('dots__dot')) {
-  //       // console.log('DOT');
-  //       const { slide } = e.target.dataset;
-  //       goToSlide(slide);
-  //       activateDot(slide);
-  //     }
-  //   });
-  // };
-
-  // slider();
 } catch (err) {
   console.log(err);
 }
