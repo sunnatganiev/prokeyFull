3e4a9debb42455464582bef724a215fe32e26a8c
vecume <shahzod.bek200109@gmail.com>
2021-06-11 20:18:10 +0500

    Fixed bug on header

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,43 +1,45 @@
-try {
-  const nav = $("#main-nav");
+jQuery(function () {
+  try {
+    const nav = $("#main-nav");
 
-  const navHeight = nav.height();
+    const navHeight = nav.height();
 
-  $(".navigation__nav").css(`top`, `${navHeight}px`);
+    $(".navigation__nav").css(`top`, `${navHeight}px`);
 
-  $("body").css(`margin-top`, `${navHeight}px`);
+    $("body").css(`margin-top`, `${navHeight}px`);
 
-  $(window).on("scroll", () => {
-    nav.toggleClass("sticky", window.scrollY > 0);
-    if (window.scrollY > 0) {
-      $("body").css(`margin-top`, `${0}px`);
-    } else {
-      $("body").css(`margin-top`, `${navHeight}px`);
-    }
-  });
+    $(window).on("scroll", () => {
+      nav.toggleClass("sticky", window.scrollY > 0);
+      if (window.scrollY > 0) {
+        $("body").css(`margin-top`, `${0}px`);
+      } else {
+        $("body").css(`margin-top`, `${navHeight}px`);
+      }
+    });
 
-  const allSections = document.querySelectorAll(".section");
+    const allSections = document.querySelectorAll(".section");
 
-  const revealSection = function (entries) {
-    const [entry] = entries;
-    // console.log(entry);
-    if (!entry.isIntersecting) return;
+    const revealSection = function (entries) {
+      const [entry] = entries;
+      // console.log(entry);
+      if (!entry.isIntersecting) return;
 
-    entry.target.classList.remove("section--hidden");
-  };
+      entry.target.classList.remove("section--hidden");
+    };
 
-  const sectionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: 0.15,
-  });
+    const sectionObserver = new IntersectionObserver(revealSection, {
+      root: null,
+      threshold: 0.15,
+    });
 
-  allSections.forEach(function (section) {
-    if (window.innerWidth > 1700) {
-      sectionObserver.observe(section);
-    } else {
-      section.classList.remove("section--hidden");
-    }
-  });
-} catch (err) {
-  console.log(err);
-}
+    allSections.forEach(function (section) {
+      if (window.innerWidth > 1700) {
+        sectionObserver.observe(section);
+      } else {
+        section.classList.remove("section--hidden");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
