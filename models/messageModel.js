be765cdff49c31c681d6602dff1fd7f4e689324d
vecume <shahzod.bek200109@gmail.com>
2021-07-09 01:00:50 +0500

    Messages bug fixed

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-const messageSchema = mongoose.Schema({
-  name: String,
-  phoneNumber: String,
-  email: String,
-  message: String,
-});
+const messageSchema = mongoose.Schema(
+  {
+    name: String,
+    phoneNumber: String,
+    email: String,
+    message: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Message = mongoose.model("Message", messageSchema);
 
